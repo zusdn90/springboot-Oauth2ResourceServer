0ba5bdeb7b756ec2df49dcf22a6f4a3645d046fc
hwhong <hwhong@bimatrix.co.kr>
2020-04-27 10:02:48 +0900

    비대칭키를 이용한 서명 방식 구현

--- src/main/java/com/rest/resource/config/Oauth2ResourceServerConfig.java
+++ src/main/java/com/rest/resource/config/Oauth2ResourceServerConfig.java
@@ -1,14 +1,19 @@
 package com.rest.resource.config;
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.IOException;
 
 @Configuration
 @EnableResourceServer
@@ -17,19 +22,6 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
     @Value("${security.oauth2.jwt.signkey}")
     private String signKey;
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
-
-        return converter;
-    }
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();    // H2 console 접근허용
@@ -37,4 +29,33 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
                 .antMatchers("/v1/users").access("#oauth2.hasScope('read')")    // read scope로 접근 허용
                 .anyRequest().authenticated();
     }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter() {
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setSigningKey(signKey);
+//
+//        return converter;
+//    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        Resource resource = new ClassPathResource("publicKey.txt");
+        String publicKey = null;
+        try {
+            publicKey = IOUtils.toString(resource.getInputStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
+        return converter;
+    }
+
+
 }
